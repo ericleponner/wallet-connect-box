8ef4666075ff6c428068bebd200241f889788857
Eric Le Ponner <eric.leponner@icloud.com>
2025-09-18 18:11:54 +0200

    Variant with @reown/appkit in place of @walletconnect/modal.
    Signed-off-by: Eric Le Ponner <eric.leponner@icloud.com>

--- src/tools.ts
+++ src/tools.ts
@@ -1,6 +1,7 @@
 import SignClient from "@walletconnect/sign-client";
 import {SessionTypes} from "@walletconnect/types";
-import {WalletConnectModal} from "@walletconnect/modal";
+import { createAppKit } from "@reown/appkit/core";
+import { hederaTestnet } from "@reown/appkit/networks";
 import {getSdkError} from "@walletconnect/utils";
 
 export const PROJECT_ID = "YOUR_PROJECT_ID"
@@ -54,19 +55,26 @@ export interface ConnectInfo {
 export async function connect(): Promise<ConnectInfo> {
     const signClient = await SignClient.init(SIGN_OPTIONS)
     const { uri, approval } = await signClient.connect(CONNECT_PARAMS)
-    const connectModal = new WalletConnectModal({
+    // const connectModal = new WalletConnectModal({
+    //     projectId: PROJECT_ID,
+    //     explorerRecommendedWalletIds: [
+    //         // https://walletguide.walletconnect.network
+    //         "a29498d225fa4b13468ff4d6cf4ae0ea4adcbd95f07ce8a843a1dee10b632f3f", // HashPack
+    //         "a9104b630bac1929ad9ac2a73a17ed4beead1889341f307bff502f89b46c8501", // Blade
+    //         "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // Metamask
+    //         "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa", // Coinbase
+    //     ]
+    // })
+    const connectModal = createAppKit({
         projectId: PROJECT_ID,
-        explorerRecommendedWalletIds: [
-            // https://walletguide.walletconnect.network
-            "a29498d225fa4b13468ff4d6cf4ae0ea4adcbd95f07ce8a843a1dee10b632f3f", // HashPack
-            "a9104b630bac1929ad9ac2a73a17ed4beead1889341f307bff502f89b46c8501", // Blade
-            "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // Metamask
-            "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa", // Coinbase
-        ]
-    })
-    await connectModal.openModal({ uri })
+        networks: [hederaTestnet],
+        manualWCControl: true
+    });
+    // await connectModal.openModal({ uri })
+    await connectModal.open({ uri })
     const session = await approval()
-    connectModal.closeModal()
+    // connectModal.closeModal()
+    await connectModal.close()
     return { signClient, session }
 }
 
